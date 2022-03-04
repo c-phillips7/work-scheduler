eaa40bd4655ef48df761302f33d693943e0b5551
Chris Phillips <cp.phillips15@gmail.com>
2022-03-04 18:02:48 -0500

    added psudeo code for tasks

---
+++ Assets/script.js
@@ -0,0 +1,15 @@
+// Set var from DOM
+const saveBtn = $('.saveBtn');
+
+// TODO: Get current date
+
+
+// TODO: function on click of save:
+// save hour and added text to local storage
+
+
+// TODO: function to load from local storage to display user inputs
+
+
+// TODO: function to highlight hour blocks based on current time
+    // done by setting class to past, present or future based on current time
